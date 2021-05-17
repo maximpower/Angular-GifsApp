922f02c331d424a22d819143a84e3825c340da2e
Maksym Yuriy Nakonechny <maxim.n.dev@gmail.com>
2021-05-17 20:48:09 +0200

    Fin sección 7: GifsApp

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent  {
+
+
+  get historial() :string[]{
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+  buscar( item:string ){
+    this.gifsService.buscarGifs(item);
+  }
+}
